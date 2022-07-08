a870214f7796e4315f3467395857d50ea5f8b010
Mingc <ciffel@gmail.com>
2022-07-08 23:03:08 +0800

    feat: remove slash command

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,9 @@ import {
 import { wikipediaParserConfig } from './parser/wikipedia-parser.js';
 import { lineTodayParserConfig } from './parser/line-today-parser.js';
 import { createMessageEmbed } from './embed.js';
-import ImageCommandHandler from './image-commands.js';
-import { SlashCommandController } from './slash-command.js';
 
 const APP_ID = process.env.DISCORD_CLIENT_ID || '';
 const BOT_TOKEN = process.env.DISCORD_TOKEN || '';
-
-const imageCommandHandler = new ImageCommandHandler();
 
 function notInQuote(str: string, position: number | undefined): boolean {
   const startIndex = str.lastIndexOf('\n', position);
@@ -26,20 +22,6 @@ function matchURL(re: RegExp, content: string): RegExpMatchArray | null {
   const match = content.match(re);
   return match && notInQuote(content, match.index) ? match : null;
 }
-
-new SlashCommandController(APP_ID, BOT_TOKEN).create({
-  name: 'post',
-  description: '貼文',
-  options: [
-    {
-      name: 'image',
-      description: '圖片名稱',
-      type: 3,
-      required: true,
-      choices: imageCommandHandler.getChoices(),
-    },
-  ],
-});
 
 const bot = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -52,19 +34,6 @@ bot
     console.log(
       `https://discord.com/api/oauth2/authorize?client_id=${APP_ID}&permissions=8192&scope=bot%20applications.commands`
     );
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    SlashCommandController.registerHandler(bot, 'post', (options: any) => {
-      if (options[0]?.name === 'image') {
-        const embed = imageCommandHandler.get(options[0]?.value);
-        return embed
-          ? {
-              content: '',
-              embeds: [embed],
-            }
-          : null;
-      }
-      return null;
-    });
   })
   .on('messageCreate', async (message) => {
     if (!message.author || message.author.bot || !message.content) {
